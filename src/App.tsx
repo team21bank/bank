faf9fddf8abe644500c3616c8b307e680c7d0a80
Jack Kingham <wowknead@gmail.com>
2022-09-29 21:50:02 -0400

    Fixed strange indentation

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import './App.css';
 import "./firebase";
 
 function App() {
-    //Example of creating a node in the database and inserting string data under it
-    //let database_reference = ref(getDatabase());
-    //let c = child(database_reference, "test");
-    //push(c, "Hello!");
+  //Example of creating a node in the database and inserting string data under it
+  //let database_reference = ref(getDatabase());
+  //let c = child(database_reference, "test");
+  //push(c, "Hello!");
 
 
-    return (
+  return (
     <div className="App">
       <header className="App-header">
         <h1>Banking Application</h1>
