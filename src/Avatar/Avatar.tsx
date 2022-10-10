11a2e6354ce8f1b34b0061739062afac93dca020
dthakar23 <dthakar@udel.edu>
2022-10-10 13:30:45 -0400

    fixed avatar

--- src/Avatar/Avatar.tsx
+++ src/Avatar/Avatar.tsx
@@ -1,29 +1,62 @@
 import { Button, Form } from "react-bootstrap";
 import React, { ChangeEvent, useRef, useState } from "react";
+import ImageUploading, { ImageListType } from "react-images-uploading";
 
-export function AvatarForm(){
-    /*
-    function uploadImage(event: React.ChangeEvent<HTMLImageElement>){
-        //HTMLImageElement???
-        setImage(event.target.src)
-    }
 
-    const [image, setImage] = useState<string>("");
-    */
+export function AvatarForm() {
 
-    const [image, setImage] = useState<[{image: object}]>([{image: {}}])
-    
-    const handleSetImage = (event: ChangeEvent<HTMLInputElement>) => {
-        const { files } = event.target;
-    }
+    const [images, setImages] = React.useState([]);
+    const maxNumber = 69;
+
+    const onChange = (
+        imageList: ImageListType,
+        addUpdateIndex: number[] | undefined
+    ) => {
+        // data for submit
+        console.log(imageList, addUpdateIndex);
+        setImages(imageList as never[]);
+    };
 
     return (
-      <div>
-        <Form.Group controlId="avatar">
-        <Form.Label>Upload Image</Form.Label>
-            <input type="file" name="image" accept="image/gif, image/jpeg, image/png" onChange={e => handleSetImage(e)}/>
-            <input type="submit"></input>
-        </Form.Group>
-      </div>
+        <div className="App">
+            <ImageUploading
+                multiple
+                value={images}
+                onChange={onChange}
+                maxNumber={maxNumber}
+            >
+                {({
+                    imageList,
+                    onImageUpload,
+                    onImageRemoveAll,
+                    onImageUpdate,
+                    onImageRemove,
+                    isDragging,
+                    dragProps
+                }) => (
+                    // write your building UI
+                    <div className="uploadimage-wrapper">
+                        <Button
+                            style={isDragging ? { color: "red" } : undefined}
+                            onClick={onImageUpload}
+                            {...dragProps}
+                            size="sm"
+                        >
+                            Upload Image
+                        </Button>
+                        <Button onClick={onImageRemoveAll} size="sm">Remove all images</Button>
+                        {imageList.map((image, index) => (
+                            <div key={index} className="image-item">
+                                <img src={image.dataURL} alt="" width="100" />
+                                <div className="image-itembtn-wrapper">
+                                    <Button onClick={() => onImageUpdate(index)}>Update</Button>
+                                    <Button onClick={() => onImageRemove(index)}>Remove</Button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </ImageUploading>
+        </div>
     );
-  }
+};
