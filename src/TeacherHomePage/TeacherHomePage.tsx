676731aa2cddb258947b05f8d7397cbf554b6015
dthakar23 <dthakar@udel.edu>
2022-10-11 18:22:57 -0400

    fixed merge conflict

--- src/TeacherHomePage/TeacherHomePage.tsx
+++ src/TeacherHomePage/TeacherHomePage.tsx
@@ -8,11 +8,7 @@ export function TeacherHomePage({passID}:
     {passID: (passID: string) => void}){
     return (<div>
         <ClassCodeForm></ClassCodeForm>
-<<<<<<< HEAD
-        <LogoutButton></LogoutButton>
-        <AvatarForm></AvatarForm>
-=======
         <LogoutButton passID={passID}></LogoutButton>
->>>>>>> ca3ab78222349204a2a6596191188f429522b735
+        <AvatarForm></AvatarForm>
     </div>)
 }
