ce2aa08f353b6bb3ead9756293f10b16deedfefc
Craig Barber <RacgiMan@icloud.com>
2022-10-31 13:28:13 -0400

    Reverted testing change to navbar; Confirmed functioning of ImgViewer, should work anywhere the user context is available by passing in the avatar field.

--- src/AvatarPool/ImageViewer.tsx
+++ src/AvatarPool/ImageViewer.tsx
@@ -1,16 +1,16 @@
 import React, { useContext, useState } from 'react';
 
-export function ImageViewer({avatar}: {avatar: string}){
+export function ImageViewer({avatar}: {avatar: string | undefined}){
     //Takes in the avatar string from a user to determine the profile picture to display.
 
     //Try catch block that'll display the user's profile picture if it exists; The default profile otherwise.
     try {
-        return <div> USER PROFILE PICTURE LOADED SUCCESSFULLY:
+        return <span>
         <img border-radius="50%" width="50px" alt="Your Avatar" src={require("./" + avatar + ".png")} ></img>
-        </div>
+        </span>
     } catch {
-        return <div> DEFAULT PROFILE PICTURE (USER FAILED):
+        return <span>
         <img border-radius="50%" width="50px" alt="Default Avatar" src={require("./default.png")} ></img>
-        </div>
+        </span>
     }
 }
