5f2887631001405282184236a8fa0ccbcd422b19
jackfk <jackfk@udel.edu>
2022-11-02 14:08:19 -0400

    css changes to registration form.

--- src/Authentication/Registration/Reg.tsx
+++ src/Authentication/Registration/Reg.tsx
@@ -77,38 +77,60 @@ export function RegistrationForm(){
     };
 
     //HTML for registration textboxes and button
-    return (<div>
-        <Form.Group controlId="registration">
-            <Form.Label>Enter Your Email</Form.Label>
-            <Form.Control
-                value={email}
-                onChange={updateEmail}/>
-            <br/>
-            <Form.Label>Enter Your Username</Form.Label>
-            <Form.Control
-                value={username}
-                onChange={updateUser}/>
-            <br/>
-            <Form.Label>Enter Your School ID</Form.Label>
-            <Form.Control
-                value={id}
-                onChange={updateId}/>
-            <br/>
-            <Form.Label>Enter Your Password</Form.Label>
-            <Form.Control
-                type="password"
-                value={p1}
-                onChange={updateP1}/>
-            <br/>
-            <Form.Label>Confirm Your Password</Form.Label>
-            <Form.Control
-                type="password"
-                value={p2}
-                onChange={updateP2}/>
-            <br/>
-            <Button onClick={register}>Register</Button>
-            <Link to="/"><Button>Back To Home</Button></Link>
-        </Form.Group>
+    return (<div className='register-page'>
+        <div className='register-header'>
+            <h1 className='register-header-text'>Register For Team21 Bank</h1>
+        </div>
+        <div className='register-forms'>
+            <Form.Group controlId="registration">
+                <div className="form-wrapper">
+                    <Form.Label className="register-form-header">Enter Your Email</Form.Label>
+                    <Form.Control
+                        className='register-text-box'
+                        placeholder='ex. example@test.com'
+                        size='lg'
+                        value={email}
+                        onChange={updateEmail}/>
+                </div>
+                <div className="form-wrapper">
+                    <Form.Label className="register-form-header">Enter Your Username</Form.Label>
+                    <Form.Control
+                        className='register-text-box'
+                        size='lg'
+                        value={username}
+                        onChange={updateUser}/>
+                </div>
+                <div className="form-wrapper">
+                    <Form.Label className="register-form-header">Enter Your School ID</Form.Label>
+                    <Form.Control
+                        className='register-text-box'
+                        size='lg'
+                        value={id}
+                        onChange={updateId}/>
+                </div>
+                <div className="form-wrapper">
+                    <Form.Label className="register-form-header">Enter Your Password</Form.Label>
+                    <Form.Control
+                        className='register-text-box'
+                        size='lg'
+                        type="password"
+                        value={p1}
+                        onChange={updateP1}/>
+                </div>
+                <div className="form-wrapper">
+                    <Form.Label className="register-form-header">Confirm Your Password</Form.Label>
+                    <Form.Control
+                        className='register-text-box'
+                        size='lg'
+                        type="password"
+                        value={p2}
+                        onChange={updateP2}/>
+                </div>
+                <br/>
+                <Button onClick={register} className="register-form-button">Register</Button>
+                <Link to="/"><Button className="register-form-button">Back To Home</Button></Link>
+            </Form.Group>
+        </div>
     </div>);
 
 }
