348f0196ef2008618fab84ebc4f5d85d0056cd2b
dthakar23 <dthakar@udel.edu>
2022-11-08 18:29:23 -0500

    quiz-view, moveToNext, sample quizzes

---
+++ src/Quizzes/QuizList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Stack } from "react-bootstrap";
+import { Quiz } from "../Interfaces/Quiz";
+import { QuizView } from "../Quizzes/QuizView";
+
+export function QuizList({
+    quizzes
+}: {
+    quizzes: Quiz[];
+}): JSX.Element {
+    return (
+        <Stack gap={3}>
+            {quizzes.map((quiz: Quiz) => (
+                <div key={quiz.title} className="bg-light border m-2 p-2">
+                    <QuizView quiz={quiz}></QuizView>
+                </div>
+            ))}
+        </Stack>
+    );
+}
---
+++ src/Quizzes/QuizMain.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Quiz } from "../Interfaces/Quiz";
+import { QuizQuestion } from "../Interfaces/QuizQuestion";
+import quizzes from "./quizData.json";
+import { QuizList } from "./QuizList";
+
+const allQuizzes = quizzes.map((myquiz): Quiz => ({ ...myquiz}));
+
+//make an import JSON feature
+export function QuizMain(): JSX.Element{
+    const [quizzes, setQuizzes] = useState<Quiz[]>(allQuizzes);
+    return(
+        <h3>
+        <QuizList
+            quizzes={allQuizzes}
+        ></QuizList>
+    </h3>
+    );
+}
+
+
