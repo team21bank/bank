9514edc5df5ed2b2659b43f7a0b22257c7aa0086
Jack Kingham <jackfk@udel.edu>
2022-11-14 13:56:24 -0500

    Fetch student's balance upon navigating to class.

--- src/HomePages/StudentClassPage/StudentClassPage.tsx
+++ src/HomePages/StudentClassPage/StudentClassPage.tsx
@@ -1,14 +1,33 @@
-import React, { useContext } from 'react';
+import { getDatabase, onValue, ref } from 'firebase/database';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from "../../Authentication/auth";
 import { LoadingPage } from "../../Authentication/LoadingPage/LoadingPage";
+import { auth } from '../../firebase';
+import { BankUser } from '../../Interfaces/BankUser';
 import "./StudentClassPage.css";
 
 export function StudentClassPage({classCode}:{classCode:string}){
     const user = useContext(AuthContext);
 
+    const [bankUser, setBankUser] = useState<BankUser | undefined>();
+    useEffect(()=>{
+        const classRef = ref(getDatabase(), "/groups/"+classCode.slice(0,6));
+        onValue(classRef, classSnapshot=>{
+            const classObj = classSnapshot.val();
+            if(classObj != null) {
+                classObj.bankObj.studentList.map(bank_user => {
+                    if(bank_user.uid === auth.currentUser?.uid) {
+                        setBankUser(bank_user);
+                    }
+                });
+            }
+        })
+    }, [user.user]);
+
     return user.user ? (
         <div className="student-class-page">
             Welcome to {classCode.slice(6)}
+            <div>your total balance is {bankUser ? bankUser.balance : "fail"}</div>
         </div>
     ) : (
         <LoadingPage/>
