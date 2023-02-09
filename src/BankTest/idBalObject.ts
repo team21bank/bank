d736a79414a7dd9c3aab250ec3ac381824a9765a
Jack Kingham <wowknead@gmail.com>
2023-02-09 15:33:38 -0500

    Began outlining common database functions.

--- src/BankTest/idBalObject.ts
+++
@@ -1,4 +0,0 @@
-export interface idBal {
-    studentId: number;
-    balance: number;
-}
---
+++ src/DatabaseFunctions/BankUserFunctions.ts
@@ -0,0 +1,17 @@
+import React from "react";
+import { BankUser } from "../Interfaces/BankUser";
+
+//Sets the BankUser object at /groups/bank_id/studentList/index_of(user_id) to new_bank_user
+export function update_bank_user(bank_id: string, user_id: string, new_bank_user: BankUser) {
+    
+}
+
+//Deletes the BankUser object at /groups/bank_id/studentList/index_of(user_id)
+export function delete_bank_user(bank_id: string, user_id: string) {
+
+}
+
+//Adds a new BankUser object with uid user_id to the end /groups/bank_id/studentList
+export function create_default_bank_user(bank_id: string, user_id: string) {
+    
+}
