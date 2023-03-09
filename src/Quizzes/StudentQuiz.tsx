3fb57a36c715a7a5b5bb8876c957c8d6d872cca2
root <root@DESKTOP-MNRFI0M>
2023-03-09 18:41:06 -0500

    Fixed studentquiz

--- src/Quizzes/StudentQuiz.tsx
+++ src/Quizzes/StudentQuiz.tsx
@@ -15,11 +15,13 @@ export function StudentQuizMain(): JSX.Element{
     const bank_context = useContext(BankContext);
     const current_bank: Bank = bank_context.bank ? bank_context.bank : DEFAULT_BANK;
 
+    let dbquizzes = current_bank.quizzes
+
     return(
         <h3>
         <QuizList
             classCode={current_bank.bankId}
-            quizzes={quizzes}
+            quizzes={dbquizzes}
         ></QuizList>
     </h3>
     
