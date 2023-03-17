806d4a5148fb38255c606b22fb9b8c25ad9f1c15
Craig Barber <RacgiMan@icloud.com>
2023-03-17 12:58:21 -0400

    Finished Student-To-Student Transaction Function. First in a family of transaction generation functions. Added better documentation for transactions and the functions related to it (using jsdoc comments)

--- src/Interfaces/Transaction.ts
+++ src/Interfaces/Transaction.ts
@@ -1,6 +1,19 @@
 import { AuthUser } from "../Authentication/auth";
 import { BankUser } from "./BankUser";
 
+/**
+ * @property {Date} date- Date Object for when the transaction occurred.
+ * @property {string} receiver_name - The username of the person receiving money
+ * @property {string} sender_name - (optional) The username of the person sending money
+ * @property {string} receiver_description - Description of the transaction from the receiver's point of view
+ * @property {string} sender_description - Description of the transaction from the sender's point of view
+ * @property {string} type - Types like: "academic" / "commerce" / "transfers" / "misc"
+ * @property {number} transfer_amount - The absolute value of the transaction amount
+ * @property {number} receiver_balance - The balance of the receiver after the transaction (i.e if their balance was 500 and they gained 50 dollars, this field would be 550)
+ * @property {number} sender_balance - (optional in case of system payouts) The balance of the sender after the transaction (i.e if their balance was 500 and they sent 50 dollars, this field would be 450)
+ * @property {string} receiver_uid - The uid of the person gaining money
+ * @property {string} sender_uid - (optional in case of system payouts) The uid of the person sending money
+ */
 export interface Transaction{
     /*
     Representation of Transactions in the database - Contains all the information you'd need about any transaction.
@@ -27,7 +40,7 @@ export interface Transaction{
 
     sender_description: string; //Desc from the perspective of the person who sent money.
 
-    type?: string; //The type of transaction it was [Is this necessary? I don't know if this means anything.
+    type?: string; //The type of transaction: academic / commerce / transfers / misc. A
 
     transfer_amount: number; //The amount of money the transaction was for
 
@@ -45,8 +58,17 @@ export interface Transaction{
 Comparison function to sort transactions by date. Passed in when you're calling .sort on an array, i.e:
 transactionList.sort((a, b) => compareDates(a, b))
 
-NOTE FOR USE: .sort() sorts from low to high, if you want to sort from oldest to newest, you'll need to reverse it
+NOTE FOR USE: .sort() sorts from low to high, if you want to sort from newest to oldest, you'll need to reverse it
 */
+/**
+ * Comparison function to sort transactions by date. Passed in when you're calling .sort on an array, i.e:
+transactionList.sort((a, b) => compareDates(a, b))
+
+NOTE FOR USE: .sort() sorts from low to high, if you want to sort from newest to oldest, you'll need to reverse it
+ * @param a Transaction 1 to be compared
+ * @param b Transaction 2 to be compared
+ * @returns 1 if a happened before b, -1 if b happened before a, and 0 if they happned at the exact same time. (used for sorting)
+ */
 export function compareDates(a: Transaction, b: Transaction) {
     if (a.date > b.date) { 
         return 1;
@@ -57,22 +79,32 @@ export function compareDates(a: Transaction, b: Transaction) {
     return 0
 }
 
-/*
-Function to create a transaction object given parameters:
-*/
-export function makeTransaction(
+/**
+ * Function to create a transaction object between 2 students given: 
+ * @param senderAuth - The AuthUser of the sender in the transaction
+ * @param senderBank - The BankUser of the sender in the transaction
+ * @param receiverAuth - The AuthUser of the receiver in the transaction
+ * @param receiverBank - The BankUser of the receiver in the transaction
+ * @param amount - The numeric amount that the transaction is for
+ * @param shopPurchase - true if this was the result of a sale of goods, sets the type to "commerce", false otherwise, sets the type to "transfer"
+ * @param receiverDesc - (optional) A string description of the transaction from the perspective of the receiver, defaults to "got paid by (username)"
+ * @param senderDesc - (optional) A string description of the transaction from the perspective of the sender, defaults to "paid (username)"
+ * @returns A transaction object corresponding to the given transaction information.
+ */
+
+export function makeStudentToStudentTransaction(
     senderAuth: AuthUser, senderBank: BankUser, 
     receiverAuth: AuthUser, receiverBank: BankUser, 
-    amount: number, type?: string, 
-    receiver_desc?: string, sender_desc?: string,
+    amount: number, shopPurchase: boolean,
+    receiverDesc?: string, senderDesc?: string
     ): Transaction {
     const transaction: Transaction = {
         date: new Date(),
         receiver_name: receiverAuth.username,
         sender_name: senderAuth.username,
-        receiver_description: sender_desc || "got paid by" + senderAuth.username,
-        sender_description: receiver_desc || "paid" + receiverAuth.username,
-        type: type || "misc",
+        receiver_description: senderDesc || "got paid by" + senderAuth.username,
+        sender_description: receiverDesc || "paid" + receiverAuth.username,
+        type: shopPurchase ? "commerce" : "transfer",
         transfer_amount: amount,
         receiver_balance: receiverBank.balance + amount,
         sender_balance: senderBank.balance - amount,
