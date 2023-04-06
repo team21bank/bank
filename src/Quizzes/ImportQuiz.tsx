116826f5321c494179da4c54538db436ae6e14eb
root <root@DESKTOP-MNRFI0M>
2023-04-06 17:56:20 -0400

    cleaned up and hopefully fixed import issues

--- src/Quizzes/ImportQuiz.tsx
+++ src/Quizzes/ImportQuiz.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { ref, getDatabase, onValue, set } from '@firebase/database';
+import { ref, getDatabase, set } from '@firebase/database';
 import { Quiz } from "../Interfaces/Quiz";  
 import { QuizQuestion } from "../Interfaces/QuizQuestion";
-import { Bank } from "../Interfaces/BankObject";
+import { Bank, DEFAULT_BANK } from "../Interfaces/BankObject";
+import { BankContext } from "../Authentication/auth";
 
 export function ImportQuiz({classCode}: {classCode: string}): JSX.Element {
-    const [currClass, setCurrClass] = useState<Bank>({
-        bankId:'',
-        teacherID:'',
-        studentList:[],
-        classTitle:'',
-        quizzes:[],
-        subgroups:[]
-    });
     
-    useEffect(() => {
-        onValue(ref(getDatabase(),"/groups/"+classCode.slice(0,6)+"/bankObj"),ss=>{
-            if(currClass !== ss.val()) {
-                setCurrClass(ss.val());
-            }
-        })
-    }, [classCode, currClass]);
+    const currClass: Bank = useContext(BankContext).bank ?? DEFAULT_BANK;
 
     const [contents, setContents] = useState<string>("");
     const [view, toggleView] = useState<boolean>(false);
@@ -114,12 +101,6 @@ export function ImportQuiz({classCode}: {classCode: string}): JSX.Element {
         console.log(newQuiz);
 
         //temporary for testing purposes, replace with storage to firebase
-        if (currClass.quizzes[0].title ===''){
-            currClass.quizzes[0] = newQuiz
-        }
-        else{
-            currClass.quizzes.push(newQuiz);
-        }
         set(groupRef,currClass)
 
 
