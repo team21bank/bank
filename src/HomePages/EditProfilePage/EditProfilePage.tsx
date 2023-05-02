68dd384588f0473261b92eb0b8438c26e1d2b7b1
beckyren <beckyren@gitlab.eecis.udel.edu>
2023-05-02 13:48:29 -0400

    renaissance font and pictures

--- src/HomePages/EditProfilePage/EditProfilePage.tsx
+++ src/HomePages/EditProfilePage/EditProfilePage.tsx
@@ -11,6 +11,8 @@ import { auth } from "../../firebase";
 import { ImageGallery } from "../../AvatarPool/ImageGallery";
 import { DEFAULT_AUTH_USER } from "../../Interfaces/AuthUser";
 import { update_auth_user } from "../../DatabaseFunctions/UserFunctions";
+import MonaLisa from "../../Images/MonaLisa.jpg";
+import Leo from "../../Images/LeoDaVinci.jpg"
 
 
 export function EditProfile(): JSX.Element {
@@ -26,7 +28,12 @@ export function EditProfile(): JSX.Element {
     const home_page = user.isTeacher ? "/teachers/home" : "/students/home";
 
     return (
-        <div className="edit-profile-page">
+        <div id = "parchmentBackground" className="edit-profile-page-row">
+
+            <div >
+                <img src={MonaLisa} className="edit-profile-page" />
+            </div>
+            <div className="edit-profile-page">
             <h1>Edit Profile</h1>
             <div>Logged in as user {user.username}</div>
 
@@ -40,6 +47,10 @@ export function EditProfile(): JSX.Element {
             <Link to={home_page}><Button>Cancel Changes</Button></Link>
 
             <DeleteAccountModal />
+         </div>
+            <div >
+                <img src={Leo} className="edit-profile-page" />
+            </div>
         </div>
     )
 }
