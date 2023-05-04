4d4d03e83f849d5e8898ea02c3ec90a4d54b6cda
Craig Barber <RacgiMan@icloud.com>
2023-05-04 19:40:31 -0400

    Toggling Font On/Off

--- src/HomePages/DefaultHomePage/DefaultHomePage.tsx
+++ src/HomePages/DefaultHomePage/DefaultHomePage.tsx
@@ -4,25 +4,6 @@ import React, { useState } from "react";
 import "./DefaultHomePage.css";
 
 export function DefaultHomePage(): JSX.Element {
-  //@ts-expect-error
-  const globalFontStylesheet = [...document.styleSheets].find((sheet) => 
-  //@ts-expect-error
-  [...sheet.cssRules].reduce((rules: String, curRule: CSSRule) => curRule.style ? rules += curRule.style.cssText : "", "").includes("appcss"));
-  
-  const bodyRule = globalFontStylesheet ? [...globalFontStylesheet.cssRules].find(
-    (r) => r.selectorText === "body"): null;
-    
-  const codeRule = globalFontStylesheet ? [...globalFontStylesheet.cssRules].find(
-    (r) => r.selectorText === "code") : null;
-
-  function disableGlobalFont(): void {
-    if (bodyRule) {
-      bodyRule.style.setProperty("font-family", "Helvetica, sans-serif");
-    }
-    if (codeRule) {
-      codeRule.style.setProperty("font-family", "Helvetica, sans-serif");
-    }
-  }
 
   return (
     <div className="home-page">
@@ -32,9 +13,8 @@ export function DefaultHomePage(): JSX.Element {
       </header>
       <Link to="/login"><Button size="lg" className="start-button">Login</Button></Link>
       <Link to="/register"><Button size="lg" className="start-button">Register</Button></Link>
-      <span style={{fontFamily: "sans-serif"}}>
-      <Button onClick={() => disableGlobalFont()}>Enable Plain Font (persists until refresh)</Button>
-      </span>
+      <div style={{fontFamily: "sans-serif"}}>
+      </div>
     </div>
   )
 }
